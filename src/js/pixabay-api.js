e63eb93b4e607027f6650025de8b1d8d496cbdb6
Lena <lena2316@ukr.net>
2025-08-01 17:50:35 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '51504630-db9fa299e862fff98d3dacf4e';
 
-export async function fetchFromPixabay(query, page = 1) {
+export async function getImagesByQuery(query, page = 1) {
   try {
     const response = await axios.get(BASE_URL, {
       params: {
