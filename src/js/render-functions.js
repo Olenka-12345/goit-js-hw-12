67f76d7863bacbf664c0cb0ccf0c77336612f877
Lena <lena2316@ukr.net>
2025-08-01 17:51:12 +0200

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,51 +1,45 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const galleryContainer = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('#load-more');
+const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
+const loadMoreBtn = document.querySelector('.load-more-btn');
 
 const lightbox = new SimpleLightbox('.gallery a');
 
 export function createGallery(images) {
-  const markup = images.map(image => `
-    <li class="gallery-item">
-      <a href="${image.largeImageURL}">
-        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
-      </a>
+  const markup = images.map(img => `
+    <a href="${img.largeImageURL}" class="photo-card">
+      <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
       <div class="info">
-        <p><b>Likes:</b> ${image.likes}</p>
-        <p><b>Views:</b> ${image.views}</p>
-        <p><b>Comments:</b> ${image.comments}</p>
-        <p><b>Downloads:</b> ${image.downloads}</p>
+        <p><b>Likes:</b> ${img.likes}</p>
+        <p><b>Views:</b> ${img.views}</p>
+        <p><b>Comments:</b> ${img.comments}</p>
+        <p><b>Downloads:</b> ${img.downloads}</p>
       </div>
-    </li>
+    </a>
   `).join('');
 
-  galleryContainer.insertAdjacentHTML('beforeend', markup);
+  gallery.insertAdjacentHTML('beforeend', markup);
   lightbox.refresh();
 }
 
 export function clearGallery() {
-  galleryContainer.innerHTML = '';
+  gallery.innerHTML = '';
 }
 
-
 export function showLoader() {
-  const loader = document.querySelector('.loader');
-  if (loader) loader.classList.remove('hidden');
+  loader.classList.add('visible');
 }
 
 export function hideLoader() {
-  const loader = document.querySelector('.loader');
-  if (loader) loader.classList.add('hidden');
+  loader.classList.remove('visible');
 }
 
-
 export function showLoadMoreButton() {
-  loadMoreBtn.classList.remove('hidden');
+  loadMoreBtn.classList.add('visible');
 }
 
 export function hideLoadMoreButton() {
-  loadMoreBtn.classList.add('hidden');
+  loadMoreBtn.classList.remove('visible');
 }
