8ed110a1dc6d2430841d2e05e8604fdd62d90ed1
Lena <lena2316@ukr.net>
2025-08-01 13:16:18 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,25 +1,3 @@
-import { getImagesByQuery } from './js/pixabay-api.js';
-
-import {
-  createGallery,
-  clearGallery,
-  showLoader,
-  hideLoader,
-  showLoadMoreButton,
-  hideLoadMoreButton
-} from './js/render-functions.js';
-
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-const loadMoreBtn = document.querySelector('#load-more');
-
-let query = '';
-let page = 1;
-let totalPages = 0;
-
 form.addEventListener('submit', async e => {
   e.preventDefault();
   query = input.value.trim();
@@ -32,10 +10,11 @@ form.addEventListener('submit', async e => {
 
   clearGallery();
   hideLoadMoreButton();
+  
 
   try {
     const data = await getImagesByQuery(query, page);
-    hideLoader();
+    hideLoader(); // на випадок, якщо залишився
 
     if (data.totalHits === 0) {
       iziToast.error({ message: 'No images found' });
@@ -53,34 +32,3 @@ form.addEventListener('submit', async e => {
     iziToast.error({ message: 'Error loading images' });
   }
 });
-
-loadMoreBtn.addEventListener('click', async () => {
-  page++;
-  showLoader();
-
-  try {
-    const data = await getImagesByQuery(query, page);
-    hideLoader();
-    createGallery(data.hits);
-    scrollGallery();
-
-    if (page >= totalPages) {
-      hideLoadMoreButton();
-      iziToast.info({ message: "We're sorry, but you've reached the end of search results." });
-    }
-  } catch {
-    hideLoader();
-    iziToast.error({ message: 'Failed to load more images' });
-  }
-});
-
-function scrollGallery() {
-  const card = document.querySelector('.gallery-item');
-  if (!card) return;
-
-  const height = card.getBoundingClientRect().height;
-  window.scrollBy({
-    top: height * 2,
-    behavior: 'smooth'
-  });
-}
